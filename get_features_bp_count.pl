294d88407c49073a83a94f55c5cf1a454d5a581e
Sofia Robb <sofia2341@gmail.com>
2011-12-13 08:56:17 -0800

    daily commit of any changes to all scritps

--- get_features_bp_count.pl
+++ get_features_bp_count.pl
@@ -3,14 +3,22 @@ use strict;
 use Bio::DB::SeqFeature::Store;
 use Data::Dumper;
 use List::Util qw(first max maxstr min minstr reduce shuffle sum);
+## sqlite data file path
+## ex:/home_stajichlab/robb/rice/database/MSUr7.mping.insertions.sqlite
+my $sqlite = shift; 
+
 
 # Open the sequence database
 my $db = Bio::DB::SeqFeature::Store->new(-adaptor => 'DBI::SQLite',
-                                           -dsn     => '/home_stajichlab/robb/rice/database/MSUr7.mping.insertions.sqlite');
+                                           -dsn     => $sqlite);
 my %ref;
 my $ref_total = 0;
 my %transposons;
+##change chromosome to your reference type, ex: scaffold
 my @features_type = $db->get_features_by_type('chromosome');
+
+##if there are any chromosomes or scaffolds you want to skip,
+##put the names in the regex
 foreach my $feature (@features_type) {
   my $ref = $feature->ref;
   next if $ref =~/Un/ or $ref =~ /Sy/;
@@ -20,6 +28,8 @@ foreach my $feature (@features_type) {
 }
 print "ref: $ref_total\n";
 my %feat;
+
+##change the @queries list to features you want to count total nucleotides
 my @queries = qw (gene cds three_prime_UTR five_prime_UTR);
 foreach my $query (@queries){
   foreach my $feature ($db->get_features_by_type($query)) {
@@ -29,19 +39,28 @@ foreach my $query (@queries){
         ${$feat{$query}{$ref}}[$i] = 0;
       }
     }
+	
 ### do not count anything that belongs to a gene that is annotated as a transposon
-    my %attr = $feature->attributes;
-    if ($query eq 'gene'){
-      my $notes_arry_ref = $attr{'Note'};
-      my $note = ${$notes_arry_ref}[0];
-      my $name = ${$attr{'load_id'}}[0];
-      $transposons{$name}=0 if $note =~ /transposon/;
-      next if $note =~ /transposon/;
-    }else{
-      my $name_arry_ref = $attr{'parent_id'};
-      my $name = ${$name_arry_ref}[0];
-      $name =~ s/(\w+)\.\d+?$/$1/;
-      next if exists $transposons{$name};    
+##change this to =0 if you want to count transposons
+my $trasposonCheck = 1;
+
+##I do not want to count any exons or other features that belong
+##to transposons. determined by the word 'transposon' appearing in
+##the 9th column Note=
+    if ($transponCheck){
+      my %attr = $feature->attributes;
+      if ($query eq 'gene'){
+        my $notes_arry_ref = $attr{'Note'};
+        my $note = ${$notes_arry_ref}[0];
+        my $name = ${$attr{'load_id'}}[0];
+        $transposons{$name}=0 if $note =~ /transposon/;
+        next if $note =~ /transposon/;
+      }else{
+        my $name_arry_ref = $attr{'parent_id'};
+        my $name = ${$name_arry_ref}[0];
+        $name =~ s/(\w+)\.\d+?$/$1/;
+        next if exists $transposons{$name};    
+      }
     }
 ## end transposon check
 
--- sync_commit.sh
+++ sync_commit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ## this is to automate the committing of any new changes to my git repository
-echo "----------" >> sync_commit.log
-date >> sync_commit.log
-echo "---------" >> sync_commit.log
+echo "----------" >> ~/sync_commit.log
+date >> ~/sync_commit.log
+echo "---------" >> ~/sync_commit.log
 cd ~/src/scripts_by_sofia
 rsync -u ~/bin/* ~/src/scripts_by_sofia/.
-git add *
-git status >> sync_commit.log
-git commit -m 'daily commit of any changes to all scritps'
-git push
+/usr/local/bin/git add *
+/usr/local/bin/git status >> ~/sync_commit.log
+/usr/local/bin/git commit -m 'daily commit of any changes to all scritps'
+/usr/local/bin/git push
