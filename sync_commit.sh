614e81e56e78a747c55cc430fb5cc67feff74bbb
Sofia Robb <sofia2341@gmail.com>
2013-07-23 00:01:25 -0700

    daily commit of any changes to all scritps

--- sync_commit.sh
+++ sync_commit.sh
@@ -14,13 +14,19 @@ rsync -uL ~/bin/find_mping_insertions/* ~/src/scripts_by_sofia/find_mping_insert
 rsync -uL ~/bin/gff_and_seqfeaturestore_scripts/* ~/src/scripts_by_sofia/gff_and_seqfeaturestore_scripts/.
 rsync -uL ~/bin/process_raw_paired_reads_2_split_by_target/* ~/src/scripts_by_sofia/process_raw_paired_reads_2_split_by_target/.
 rsync -uL ~/bin/dynGenomClass/* ~/src/scripts_by_sofia/dynGenomClass/.
-rsync -uL ~/bin/relocaTE/* ~/src/scripts_by_sofia/relocaTE/.
-rsync -uL ~/bin/relocaTE_editing/* ~/src/scripts_by_sofia/relocaTE_editing/.
 rsync -uL ~/bin/maize/* ~/src/scripts_by_sofia/maize/.
 rsync -uL ~/bin/SNPs/* ~/src/scripts_by_sofia/SNPs/.
 rsync -uL ~/bin/pong/* ~/src/scripts_by_sofia/pong/.
 rsync -uL ~/bin/andrii/* ~/src/scripts_by_sofia/andrii/.
 rsync -uL ~/bin/emptySites/* ~/src/scripts_by_sofia/emptySites/.
+rsync -uL ~/bin/fq_indexing_tests/* ~/src/scripts_by_sofia/fq_indexing_tests/.
+rsync -uL ~/bin/mini_assemblies_and_bp_freq/* ~/src/scripts_by_sofia/mini_assemblies_and_bp_freq/.
+rsync -uL ~/bin/miscellaneous/* ~/src/scripts_by_sofia/miscellaneous/process_454/.
+rsync -uL ~/bin/process_454/* ~/src/scripts_by_sofia/process_454/.
+rsync -uL ~/bin/process_reads/* ~/src/scripts_by_sofia/process_reads/.
+rsync -uL ~/bin/utilities/* ~/src/scripts_by_sofia/utilities/.
+rm -f *~
+
 
 ##TEamRice
 #rsync -uL ~/bin/find_mping_insertions/* ~/src/TEamRice/find_mping_insertions/.
