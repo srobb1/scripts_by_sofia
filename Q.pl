a5f02303e7d2a06d385e37d2e488a07f0a176b48
Sofia Robb <sofia2341@gmail.com>
2013-07-29 00:01:08 -0700

    daily commit of any changes to all scritps

--- Q.pl
+++ Q.pl
@@ -1,8 +1,16 @@
 #!/usr/bin/perl -w
 use strict;
 use Data::Dumper;
-## echo "running:" ; qstat -u robb -t | awk {'print $10'} | grep -c R ;  echo "Waiting:" ; qstat -urobb -t | awk {'print $10'}| grep -c Q ; echo "Holding:" ; qstat -urobb -t | awk {'print $10'} | grep -c H 
 
+#torque01.int.bioinfo.ucr.edu:
+#                                                                               Req'd    Req'd      Elap
+#Job ID               Username    Queue    Jobname          SessID NDS   TSK    Memory   Time   S   Time
+#-------------------- ----------- -------- ---------------- ------ ----- ------ ------ -------- - --------
+#4645[2639].torqu     robb        js       run_split_blast_   1628     1      4    --       --  C 00:01:58
+#4645[2640].torqu     robb        js       run_split_blast_   1631     1      4    --       --  C 00:02:04
+#4645[2641].torqu     robb        js       run_split_blast_   1691     1      4    --       --  C 00:02:57
+#4645[2642].torqu     robb        js       run_split_blast_   2057     1      4    --       --  C 00:01:21
+#4645[2643].torqu     robb        js       run_split_blast_   2118     1      4    --       --  C 00:02:40
 
 my $now_string = localtime;
 print "$now_string:\n";
@@ -12,7 +20,7 @@ foreach  my $line (@jobs){
   chomp $line;
   next if $line !~ /^\d/;
   my @line = split /\s+/ , $line;
-  my $proc = $line[6];
+  my $proc = $line[6] ne '--' ? $line[6] : 1;
   $status{$line[2]}{$line[9]}+=$proc;
 }
 print Dumper \%status;
