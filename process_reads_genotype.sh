19eb3531482061c83e2d493ec78ecc871570d73b
Sofia Robb <sofia2341@gmail.com>
2013-10-04 00:01:06 -0700

    daily commit of any changes to all scritps

--- process_reads_genotype.sh
+++ process_reads_genotype.sh
@@ -176,21 +176,27 @@ vcf-to-tab < $BASE.genotype.vcf > $BASE.genotype.tab
 
 
 ## clean up
-#FILESIZE=$(stat -c%s "$BASE.genotype.vcf")
-#if [[ $FILESIZE > 5000 ]]; then
-#  echo "Cleaning up: removing files"
-#  rm $BASE.realign.bai
-#  rm $BASE.realign.bam
-#  rm $BASE.dedup.metrics
-#  rm $BASE.dedup.bai
-#  rm $BASE.dedup.bam
-#  rm $BASE.recal_data.grp
-#  rm $BASE.bam
-#  rm $BASE.bai
-#  rm $BASE.sam
-#  rm $BASE.intervals
-#  rm ${BASE}_p2.sai
-#  rm ${BASE}_p1.sai
-#  rm $BASE.RG.bai
-#  rm $BASE.RG.bam
-#fi
+FILESIZE=$(stat -c%s "$BASE.genotype.vcf")
+if [[ $FILESIZE > 5000 ]]; then
+  if [ -e trash ] ; then
+    mkdir trash
+  fi
+  echo "Cleaning up: 
+Moving files to trash directroy. 
+If you are happy will the final results all files in trash can be deleted"
+ 
+  mv $BASE.realign.bai trash
+  mv $BASE.realign.bam trash
+  mv $BASE.dedup.metrics trash
+  mv $BASE.dedup.bai trash 
+  mv $BASE.dedup.bam trash
+  mv $BASE.recal_data.grp trash
+  mv $BASE.bam trash
+  mv $BASE.bai trash
+  mv $BASE.sam trash 
+  mv $BASE.intervals trash 
+  mv ${BASE}_p2.sai trash
+  mv ${BASE}_p1.sai trash
+  mv $BASE.RG.bai trash 
+  mv $BASE.RG.bam trash
+fi
