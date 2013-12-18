08a27583e2e78a42484068055189b48d19300387
Sofia Robb <sofia2341@gmail.com>
2013-12-18 00:01:08 -0800

    daily commit of any changes to all scritps

---
+++ clean_up_after_process_reads.sh
@@ -0,0 +1,16 @@
+BASE=$1
+
+  mv $BASE.realign.bai trash
+  mv $BASE.realign.bam trash
+  mv $BASE.dedup.metrics trash
+  mv $BASE.dedup.bai trash
+  mv $BASE.dedup.bam trash
+  mv $BASE.recal_data.grp trash
+  mv $BASE.bam trash
+  mv $BASE.bai trash
+  mv $BASE.sam trash
+  mv $BASE.intervals trash
+  mv ${BASE}_p2.sai trash
+  mv ${BASE}_p1.sai trash
+  mv $BASE.RG.bai trash
+  mv $BASE.RG.bam trash
