b6fce0e7e8a23d6a460a9e8c1aaecef20e837819
Sofia Robb <sofia2341@gmail.com>
2014-06-23 00:01:05 -0700

    daily commit of any changes to all scritps

--- clean_up_after_process_reads.sh
+++ clean_up_after_process_reads.sh
@@ -1,16 +1,18 @@
-BASE=$1
+cd /rhome/robb/Wessler-Rice/RIL/genotypes/MSU_r7.corrected
 
-  mv $BASE.realign.bai trash
-  mv $BASE.realign.bam trash
-  mv $BASE.dedup.metrics trash
-  mv $BASE.dedup.bai trash
-  mv $BASE.dedup.bam trash
-  mv $BASE.recal_data.grp trash
-  mv $BASE.bam trash
-  mv $BASE.bai trash
-  mv $BASE.sam trash
-  mv $BASE.intervals trash
-  mv ${BASE}_p2.sai trash
-  mv ${BASE}_p1.sai trash
-  mv $BASE.RG.bai trash
-  mv $BASE.RG.bam trash
+if [ ! -d trash ] ; then 
+  mkdir trash
+fi
+
+  mv *.realign.bai trash
+  mv *.realign.bam trash
+  mv *.dedup.metrics trash
+  mv *.dedup.bai trash
+  mv *.dedup.bam trash
+  mv *.recal_data.grp trash
+  mv *.sam trash
+  mv *.intervals trash
+  mv *_p2.sai trash
+  mv *_p1.sai trash
+  mv *.RG.bai trash
+  mv *.RG.bam trash
